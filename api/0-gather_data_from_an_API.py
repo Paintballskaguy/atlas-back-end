a483dc56420b0a15b365d0824c12de41ad54921a
Paintballskaguy <Johnstournament@gmail.com>
2024-11-07 12:44:02 -0600

    documented

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,33 +3,37 @@
 import sys
 import requests
 """
-Script to retrieve employee info from the json place holder api.    
+Script to retrieve employee info from the json place holder api.
 """
 
+
 def fetch_employee_todo_progress(employee_id):
+    """_summary_
+
+    Args:
+        employee_id (_type_): _description_
+    """
     user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     user_response = requests.get(user_url)
-
-
     if user_response.status_code != 200:
         print("Employee not found")
         return
-    
+
     employee_name = user_response.json().get("name")
 
     todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
     todos_response = requests.get(todos_url)
     todos = todos_response.json()
-    
 
     total_tasks = len(todos)
     done_tasks = [task for task in todos if task.get("completed")]
     number_of_done_tasks = len(done_tasks)
 
     print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
-    
+
     for task in done_tasks:
         print(f"\t {task.get('title')}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
