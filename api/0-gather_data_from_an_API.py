9aeb75ed2431099c10a0070bb1c1407c0d96d074
Paintballskaguy <Johnstournament@gmail.com>
2024-11-07 12:39:17 -0600

    task 0 works locally!

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import sys
+import requests
+"""
+Script to retrieve employee info from the json place holder api.    
+"""
+
+def fetch_employee_todo_progress(employee_id):
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    user_response = requests.get(user_url)
+
+
+    if user_response.status_code != 200:
+        print("Employee not found")
+        return
+    
+    employee_name = user_response.json().get("name")
+
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    todos_response = requests.get(todos_url)
+    todos = todos_response.json()
+    
+
+    total_tasks = len(todos)
+    done_tasks = [task for task in todos if task.get("completed")]
+    number_of_done_tasks = len(done_tasks)
+
+    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
+    
+    for task in done_tasks:
+        print(f"\t {task.get('title')}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./script_name.py <employee_id>")
+    else:
+        try:
+            employee_id = int(sys.argv[1])
+            fetch_employee_todo_progress(employee_id)
+        except ValueError:
+            print("Employee ID must be an integer.")
