1d778ba2b26a3ea5eccd3c50a1f97abb581ba223
Paintballskaguy <Johnstournament@gmail.com>
2024-11-08 10:31:43 -0600

    create the json export method. fixed pycode and some formating

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+
+"""
+This script retrieves and displays an employee's TODO list progress
+using the JSONPlaceholder API. It accepts an employee ID as a parameter
+and outputs the completed tasks and the overall task progress.
+"""
+
+import csv
+import json
+import requests
+import sys
+
+
+def fetch_employee_todo_progress(employee_id):
+    """
+    Retrieves and displays the TODO list progress for a given employee.
+
+    Args:
+        employee_id (int): The ID of the employee.
+    """
+    user_url = (
+        f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+        )
+    user_response = requests.get(user_url)
+    if user_response.status_code != 200:
+        print("Employee not found")
+        return
+
+    user_data = user_response.json()
+    employee_name = user_response.json().get("name")
+    username = user_data.get("username")
+
+    todos_url = (
+        f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+        )
+    todos_response = requests.get(todos_url)
+    todos = todos_response.json()
+
+    total_tasks = len(todos)
+    done_tasks = [task for task in todos if task.get("completed")]
+    number_of_done_tasks = len(done_tasks)
+
+    print(
+        f"Employee {employee_name} is done with tasks("
+        f"{number_of_done_tasks}/{total_tasks}):"
+        )
+
+    for task in done_tasks:
+        print(f"\t {task.get('title')}")
+
+    csv_filename = f"{employee_id}.csv"
+    with open(csv_filename, mode="w", newline="") as csv_file:
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            csv_writer.writerow([
+                employee_id,
+                username,
+                task["completed"],
+                task["title"]
+                ])
+
+    json_data = {
+        str(employee_id): [
+            {
+            "task": task["title"],
+            "completed":task["completed"],
+            "username": username
+            }
+        ]
+    }
+
+    json_filename = f"{employee_id}.json"
+    with open(json_filename, mode="w") as json_file:
+        json.dump(json_data, json_file)
+
+    print(f"Data exported to {csv_filename} and {json_filename}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./script_name.py <employee_id>")
+    else:
+        try:
+            employee_id = int(sys.argv[1])
+            fetch_employee_todo_progress(employee_id)
+        except ValueError:
+            print("Employee ID must be an integer.")
